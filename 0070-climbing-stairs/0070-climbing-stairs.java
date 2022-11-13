dec7859bd5e4cf5dbb3c05f67ddde244f6c2a9a2
Mithra <50359897+GsnMithra@users.noreply.github.com>
2022-11-13 16:02:57 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (37.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,15 +7,11 @@ class Solution {
     
     public int helper (int n) {
         if (cache[n] != -1) { return cache[n]; }
-        if (n <= 1) {
+        if (n <= 3) {
             return n;
         }
-        if (n > 2) {
-            cache[n] = helper(n - 1) + helper(n - 2);
-            return cache[n];
-        }
         else {
-            cache[n] = 1 + helper(n - 1) + helper(n - 2);
+            cache[n] = helper(n - 1) + helper(n - 2);
             return cache[n];
         }
     }
