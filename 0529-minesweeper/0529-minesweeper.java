97285a54a00c27f2ffecd3d7c186c8ef196a43fb
Mithra <50359897+GsnMithra@users.noreply.github.com>
2023-10-12 16:54:16 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (18.48%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.java
+++ 0529-minesweeper/0529-minesweeper.java
@@ -1,46 +1,76 @@
+// [["E","E","E","E","E","E","E","E"],
+//  ["E","E","E","E","E","E","E","M"],
+//  ["E","E","M","E","E","E","E","E"],
+//  ["M","E","E","E","E","E","E","E"],
+//  ["E","E","E","E","E","E","E","E"],
+//  ["E","E","E","E","E","E","E","E"],
+//  ["E","E","E","E","E","E","E","E"],
+//  ["E","E","M","M","E","E","E","E"]]
+// [0,0]
+
+// [["B","B","B","B","B","B","1","E"],
+//  ["B","1","1","1","B","B","1","M"],
+//  ["1","E","M","1","B","B","1","1"],
+//  ["M","E","1","1","B","B","B","B"],
+//  ["1","1","B","B","B","B","B","B"],
+//  ["B","B","B","B","B","B","B","B"],
+//  ["B","1","2","2","1","B","B","B"],
+//  ["B","1","M","M","1","B","B","B"]]
+ 
+// [["B","B","B","B","B","B","1","E"],
+//  ["B","1","1","1","B","B","1","M"],
+//  ["1","2","M","1","B","B","1","1"],
+//  ["M","2","1","1","B","B","B","B"],
+//  ["1","1","B","B","B","B","B","B"],
+//  ["B","B","B","B","B","B","B","B"],
+//  ["B","1","2","2","1","B","B","B"],
+//  ["B","1","M","M","1","B","B","B"]]
+
 class Solution {
-    public int mineCount(char[][] board, int i, int j) {
+    public int mineCount (char [] board [], int i, int j) {
+        i -= 1;
+        j -= 1;
+        
         int mines = 0;
-        for (int r = -1; r <= 1; r++) {
-            for (int c = -1; c <= 1; c++) {
-                int newRow = i + r;
-                int newCol = j + c;
-                if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board[newRow][newCol] == 'M') {
-                    mines++;
-                }
+        for (int c = i; c < i + 3; c += 1) {
+            for (int r = j; r < j + 3; r += 1) {
+                if (c >= 0 && c < board.length && r >= 0 && r < board[0].length && board[c][r] == 'M')
+                    mines += 1;
             }
         }
+        
         return mines;
     }
-
-    public void mineSweepUtil(char[][] board, int i, int j) {
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'E') {
+    
+    public void mineSweepUtil (char [] board [], int i, int j) {
+        if (Math.min (i, j) < 0 || i >= board.length || j >= board[0].length  || board[i][j] != 'E')
             return;
-        }
-
-        int currMines = mineCount(board, i, j);
-        if (currMines != 0) {
+        
+        int currMines = mineCount (board, i, j);
+        if (currMines != 0)
             board[i][j] = (char) (currMines + '0');
-        } else {
+        else {
             board[i][j] = 'B';
-            for (int r = -1; r <= 1; r++) {
-                for (int c = -1; c <= 1; c++) {
-                    mineSweepUtil(board, i + r, j + c);
-                }
+        
+            int [] dx = new int [] { 0, 0, 1, -1, -1, 1, -1, 1 };
+            int [] dy = new int [] { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+            for (int s = 0; s < 8; s += 1) {
+                int newX = i + dx[s];
+                int newY = j + dy[s];
+
+                mineSweepUtil (board, newX, newY);
             }
         }
     }
-
-    public char[][] updateBoard(char[][] board, int[] click) {
-        int row = click[0];
-        int col = click[1];
-
-        if (board[row][col] == 'M') {
-            board[row][col] = 'X';
-        } else {
-            mineSweepUtil(board, row, col);
+    
+    public char [][] updateBoard (char [] board [], int [] click) {
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
         }
-
+        
+        mineSweepUtil (board, click[0], click[1]);
         return board;
     }
 }
